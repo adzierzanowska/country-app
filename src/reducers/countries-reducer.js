06700520c40eb7094dda770f802f7fdf59873ae2
Amanda Dzierżanowska <dzierzanowska.amanda@gmail.com>
2018-07-24 13:55:42 +0200

    Add new functions

--- src/reducers/countries-reducer.js
+++ src/reducers/countries-reducer.js
@@ -1,8 +1,16 @@
-import { GET_COUNTRIES } from '../actions/actions-countries';
+import {
+    DELETE_COUNTRY,
+    GET_COUNTRIES,
+    GET_COUNTRY,
+    SEARCH_COUNTRIES,
+    SET_CONTINENT,
+} from '../actions/actions-countries';
 import countriesData from '../data/countries.json';
 
 const initialState = {
-    countries: countriesData
+    countries: countriesData,
+    selectedCountry: {},
+    visibleCountries: [],
 };
 
 const countriesReducer = (state = initialState, action) => {
@@ -10,10 +18,34 @@ const countriesReducer = (state = initialState, action) => {
         case GET_COUNTRIES:
             return Object.assign({}, state, {countries: state.countries});
 
+        case GET_COUNTRY:
+            const selectedCountry = state.countries.find(
+                country => country.id == action.id);
+            return Object.assign({}, state, {selectedCountry});
+
+        case SEARCH_COUNTRIES:
+            const foundCountries = state.countries.filter(
+                country => country.name.toLowerCase().includes(action.searchText.toLowerCase()));
+            return Object.assign({}, state, {visibleCountries: foundCountries});
+
+        case DELETE_COUNTRY:
+            const notDeletedCountries = state.countries.filter(
+                country => country.id != action.id);
+            const notDeletedVisibleCountries = state.visibleCountries.filter(
+                country => country.id != action.id);
+            return Object.assign({}, state, {
+                countries: notDeletedCountries,
+                visibleCountries: notDeletedVisibleCountries,
+            });
+        case SET_CONTINENT:
+            const continentCountries = state.countries.filter(
+                country => country.continent === action.name);
+            return Object.assign({}, state,
+                {visibleCountries: continentCountries});
+
         default:
             return state;
     }
-
 
 };
 
